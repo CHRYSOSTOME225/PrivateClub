76a05836d275024f0872ec1f81987a3c2bfc2d1a
kouadio kouadio jean chrysostome <gidmalebolosse@gmail.com>
2026-09-23 12:37:34 +0000

    gestion automatique de l expiration du JWT

--- src/app/interceptors.ts
+++ src/app/interceptors.ts
@@ -1,18 +1,48 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+import { Auth } from './services/auth';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
+  const auth = inject(Auth);
+  const router = inject(Router);
+
   const token = localStorage.getItem('token');
 
+  let requete = req;
+
   if (token) {
-    const requeteAvecToken = req.clone({
+
+    requete = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`
       }
     });
 
-    return next(requeteAvecToken);
   }
 
-  return next(req);
+  return next(requete).pipe(
+
+    catchError((erreur) => {
+
+      if (erreur.status === 401) {
+
+        console.log(
+          'Session expirée ou token invalide'
+        );
+
+        auth.deconnecter();
+
+        router.navigate(['/']);
+
+      }
+
+      return throwError(() => erreur);
+
+    })
+
+  );
+
 };
